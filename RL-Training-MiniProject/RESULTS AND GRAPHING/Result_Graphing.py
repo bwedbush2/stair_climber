97d147109fc93cfed952648e7a7d149cb6fe3c0d
andywang2017 <144642540+andywang2017@users.noreply.github.com>
2025-12-19 15:07:40 -0800

    data organization and graphing

--- RL-Training-MiniProject/RESULTS AND GRAPHING/Result_Graphing.py
+++ RL-Training-MiniProject/RESULTS AND GRAPHING/Result_Graphing.py
@@ -4,24 +4,16 @@ from tensorboard.backend.event_processing.event_accumulator import EventAccumula
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 problem1_folder = os.path.join(script_dir, "Problem 1")
-problem2_folder = os.path.join(script_dir, "Problem 2")
+problem2_folder = os.path.join(script_dir, "Problem 2", "With Gait and Obs")
+problem2_noObs_folder = os.path.join(script_dir, "Problem 2", "With Gait No Obs")
 problem3_folder = os.path.join(script_dir, "Problem 3")
 
 PRB1_PATH = os.path.join(problem1_folder, "events.out.tfevents.1766105278.677a1d26499e.1464.0")
 PRB2_PATH = os.path.join(problem2_folder, "events.out.tfevents.1766103354.d55c2424f1bd.13450.0")
+PRB2_NOOBS_PATH = os.path.join(problem2_noObs_folder, "events.out.tfevents.1766133570.c23b4d174978.5433.0")
 PRB3_PATH = os.path.join(problem3_folder, "ray_simulation.xml")
 
-def get_data(tag):
-    if tag in tags:
-        events = event_acc.Scalars(tag)
-        steps = [e.step for e in events]
-        values = [e.value for e in events]
-        return steps, values
-    else:
-        print(f"Warning: Tag '{tag}' not found.")
-        return [], []
-
-file_path = PRB2_PATH
+file_path = PRB1_PATH
 
 # 1. Load the data
 print(f"Loading {file_path}...")
@@ -71,12 +63,6 @@ le_values = [e.value for e in le_events]
 ae_events = event_acc.Scalars(ang_err_tag)
 ae_steps = [e.step for e in ae_events]
 ae_values = [e.value for e in ae_events]
-
-# Slip Stuff
-slip_events = event_acc.Scalars(slip_metric_tag)
-slip_steps1 = [e.step for e in slip_events]
-slip_values1 = [e.value for e in slip_events]
-
 
 # --- FIGURE 1: Reward + Length ---
 plt.figure(figsize=(10, 8))
@@ -145,6 +131,22 @@ le_events = event_acc.Scalars(lin_err_tag)
 le_steps2 = [e.step for e in le_events]
 le_values2 = [e.value for e in le_events]
 
+file_path = PRB2_NOOBS_PATH
+
+# 1. Load the data
+event_acc = EventAccumulator(file_path)
+event_acc.Reload()
+
+# 2. Extract available tags
+tags = event_acc.Tags()['scalars']
+slip_events = event_acc.Scalars(slip_metric_tag)
+slip_steps1 = [e.step for e in slip_events]
+slip_values1 = [e.value for e in slip_events]
+
+le_events = event_acc.Scalars(lin_err_tag)
+le_steps1 = [e.step for e in le_events]
+le_values1 = [e.value for e in le_events]
+
 plt.figure(figsize=(10, 8))
 
 # Subplot 1: Foot Slip Metric
@@ -159,7 +161,7 @@ plt.gca().axes.xaxis.set_ticklabels([])
 
 # Subplot 2: Tracking Performance (Re-plotted for context)
 plt.subplot(2, 1, 2)
-plt.plot(le_steps, le_values, label='Without Critic Observations', linewidth=2)
+plt.plot(le_steps1, le_values1, label='Without Critic Observations', linewidth=2)
 plt.plot(le_steps2, le_values2, label='With Critic Observations', linewidth=2)
 plt.title('Linear Velocity Tracking Error')
 plt.ylabel('Error')
